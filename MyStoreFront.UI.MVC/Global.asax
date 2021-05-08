8df4605901290e947718b3fa866b8d2acfdd13f5
Wes Nelson <WesNelson10@outlook.com>
2021-05-08 14:24:09 -0500

    Initial Commit - Everything before Identity Samples.

---
+++ MyStoreFront.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyStoreFront.UI.MVC.MvcApplication" Language="C#" %>
---
+++ MyStoreFront.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyStoreFront.UI.MVC.Startup))]
+namespace MyStoreFront.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
